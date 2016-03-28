bfb1445f348a8ff7dd1183b0838dcb85b01c96a6
husen.m <hscrystaleng@gmail.com>
2016-03-28 16:18:33 +0700

    phpmyadmin version demo

--- phpmyadmin/docker-entrypoint.sh
+++ phpmyadmin/docker-entrypoint.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-if [ -z "$MYSQL_ROOT_PASSWORD"
+if [ -z "$HOST" ]; then
+  echo >&2 'error: database is uninitialized and HOST option is not specified '
+	exit 1
+fi
+# if [ -z "$MYSQL_PASSWORD" ]; then
+#   echo >&2 'error: database is uninitialized and password option is not specified '
+# 	exit 1
+# fi
+# if [ -z "$MYSQL_LINK" ]; then
+#   echo >&2 'error: database is uninitialized and link option is not specified '
+# 	exit 1
+# fi
